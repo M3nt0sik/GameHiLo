3344c20c0f0f3dc90223e65ffe33094e15fe21ad
Kamil <mentos.96@o2.pl>
2023-05-17 20:10:13 +0200

    Write main function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Witaj w HiLo!");
+        Console.WriteLine($"Odgadnij liczbe do {HiLOGame.MAXIMUM}.");
+        HiLoGame.Hint();
+        while (HiLoGame.GetPot() > 0)
+        {
+            Console.WriteLine("Wcisnij w (wieksza), m (mniejsza) lub ? (kupno wskazowki).");
+            Console.WriteLine($"Inne klawisze to koniec gry z {HiLoGame.GetPot()} zl.");
+            char key = Console.ReadKey(true).KeyChar;
+            if (key == 'w') HiLoGame.Guess(true);
+            else if (key == 'm') HiLoGame.Guess(false);
+            else if (key == '?') HiLoGame.Hint();
+            else return;
+        }
+        Console.WriteLine("Skonczyly ci sie pieniadze. Do zobaczenia!");
+    }
+}
