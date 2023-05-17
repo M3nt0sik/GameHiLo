8d3b0bbad287a1cff70735411e3de294b4ee6c6f
Kamil <mentos.96@o2.pl>
2023-05-17 21:37:49 +0200

    Write function Guess in HiLoGame

---
+++ HiLoGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHiLo
+{
+     internal class HiLoGame
+    {
+        static Random random = new Random();
+        public int MAXIMUM = 10;
+        private int curentNumber = random.Next(1,11);
+        private int pot=MAXIMUM;
+
+        public void Guess( bool higher)
+        {
+            int nextNumber = random.Next(1, 11);
+            if (higher && nextNumber>=curentNumber)
+            {
+                Console.WriteLine("Zgadles!!!");
+                pot++;
+                         
+            }
+            else if(!higher && nextNumber <= curentNumber) {
+                Console.WriteLine("Zgadles!!!");
+                pot++;
+            }
+            else
+            {
+                Console.WriteLine("Niestety bledna odpowiedz");
+                pot--;
+            }
+        }
+
+
+
+        
+
+
+    }
+}
